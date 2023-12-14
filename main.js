a8478c0274dc68fa1ed309be947f1a72a9964831
angelomiko129 <angelomikobotabara021@gmail.com>
2023-12-14 15:50:47 +0800

    Update 2 files

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.getElementById("header");
+    var lastScrollPosition = window.scrollY || window.pageYOffset;
+
+    window.addEventListener("scroll", function () {
+        var currentScrollPosition = window.scrollY || window.pageYOffset;
+
+        if (currentScrollPosition > lastScrollPosition) {
+            header.style.backgroundAttachment = "scroll";
+        } else {
+            header.style.backgroundAttachment = "fixed";
+        }
+
+        lastScrollPosition = currentScrollPosition;
+    });
+});
+
 var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
 
